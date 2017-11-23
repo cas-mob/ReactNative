108e11313ee992dc49816ebe83997d656f3c22a5
Tobias Büchel <tobi.buechel@gmail.com>
2017-11-23 18:20:55 +0100

    Remove code and add todos to simplify exercise

--- App.js
+++ App.js
@@ -3,17 +3,6 @@ import ApiView from './components/Api'
 import { TabNavigator } from 'react-navigation'
 import { Text, View } from 'react-native'
 
-const RootTabs = TabNavigator({
-	Home: {
-		screen: () => <View style={{flex: 1, justifyContent: 'center'}}>
-			<Text style={{textAlign: 'center'}}>
-				Welcome to myApp!
-			</Text>
-		</View>
-	},
-	'Api & Share': {
-		screen: ApiView
-	}
-})
+const RootTabs = TabNavigator(/* todo: routeConfigMap erstellen */)
 
 export default RootTabs
